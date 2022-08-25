6cc4ff4d6c40714ed20e376c68d877144b5f9df2
Carlos Nogueira <carlosnogu8@gmail.com>
2022-08-25 17:07:46 -0300

    Exercício 18

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -4,3 +4,26 @@ const objeto = {
     username: "astrodev_labenu",
     senha: "melhorDeTodos"
 }
+
+const caixaAlta = (objeto) => {
+    let novoObjeto = {
+        ...objeto,
+        nome: objeto.nome.toUpperCase(),
+        profissao: objeto.profissao.toUpperCase(),
+        username: objeto.username.toUpperCase(),
+        senha: objeto.senha.toUpperCase() 
+    }
+    return novoObjeto
+}
+
+const textoCorrido = (objeto) => {
+    let mensagem = `${objeto.nome} ${objeto.profissao} ${objeto.username} ${objeto.senha}`
+    return mensagem
+}
+
+const imprimir = (objeto, callback) => {
+    console.log(callback(objeto)) 
+}
+
+imprimir(objeto,caixaAlta) 
+imprimir(objeto, textoCorrido)
